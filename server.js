19e0335887da66e36df0c565cd1d3063fe2bee03
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-10 18:17:33 -0300

    change status for sendStatus

--- server.js
+++ server.js
@@ -10,12 +10,27 @@ const participants = [{name: 'João', lastStatus: 12313123}];
 const messages = [{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}];
 
 app.post("/participants", (req, res) => {
-    if (!req.body.name || participants.find((participant) => participant.name === req.body.name)) {
-        return res.status(400)
-      }
-      participants.push({name: req.body.name, lastStatus: Date.now()})
-      messages.push({from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH-mm-ss')})
-     res.status(200)
+  if (
+    !req.body.name ||
+    participants.find((participant) => participant.name === req.body.name)
+  ) {
+    return res.sendStatus(400);
+  }
+  participants.push({ name: req.body.name, lastStatus: Date.now() });
+  messages.push({
+    from: "xxx",
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: dayjs(Date.now()).format("HH-mm-ss"),
+  });
+  console.log(participants)
+  res.sendStatus(200);
+});
+
+app.get("/participants", (req, res) => {
+    console.log(participants)
+  res.send(JSON.stringify(participants));
 });
 
 
