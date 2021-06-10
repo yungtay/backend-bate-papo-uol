9cc4be5adfe5d48413e6b8e26276a3606fdb19f9
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-10 17:59:39 -0300

    check if the name is already used

--- server.js
+++ server.js
@@ -10,14 +10,12 @@ const participants = [{name: 'João', lastStatus: 12313123}];
 const messages = [{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}];
 
 app.post("/participants", (req, res) => {
-    if (!req.body.name) {
-        return res
-          .status(400)
-          .send(`O campo do nome não está preenchido corretamente`);
+    if (!req.body.name || participants.find((participant) => participant.name === req.body.name)) {
+        return res.status(400)
       }
       participants.push({name: req.body.name, lastStatus: Date.now()})
       messages.push({from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH-mm-ss')})
-     res.status(200) 
+     res.status(200)
 });
 
 
