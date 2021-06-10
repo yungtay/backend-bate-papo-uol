1ae9f5170c97a50aeb9410983ba508ab56ac2e62
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-10 17:50:23 -0300

    implemented the backend logic of adding a new participant

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, () => console.log("Server Online"))
+const participants = [{name: 'João', lastStatus: 12313123}];
+const messages = [{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}];
+
+app.post("/participants", (req, res) => {
+    if (!req.body.name) {
+        return res
+          .status(400)
+          .send(`O campo do nome não está preenchido corretamente`);
+      }
+      participants.push({name: req.body.name, lastStatus: Date.now()})
+      messages.push({from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH-mm-ss')})
+     res.status(200) 
+});
+
+
+
+app.listen(4000, () => console.log("Server Online"));
